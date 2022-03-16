0f8b3c4efd3297de3646722a31c3650a222f7a00
mio kataoka <mio.kanna@gmail.com>
2022-03-16 13:22:32 -0700

    commit tree dddf33532c06a28f41c99cf97a78763d5e636a70

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+function submitData(name, email){
+    const newUser ={
+        name: name,
+        email: email,
+    };
+    const configurationObject ={
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(newUser),
+    }
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+            return(response.json());
+        })
+        .then(function (object){
+            const para = document.createElement("p");
+            para.innerHTML = object.id;
+            document.body.appendChild(para);
+        })
+        .catch(function(error) {
+            // document.body.innerHTML = "Unauthorized Access"
+            let message = document.createElement("p");
+            message.innerHTML = "Unauthorized Access";
+            document.body.appendChild(message)
+        })
+    }
+    submitData("mio", )
+    
